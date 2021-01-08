f9df0b421ae0ebad44745d6b89ea2f283e648e02
Endre Bock <endre.bock@codefrog.de>
2021-01-08 19:19:01 +0100

    EKZ-4 The scaffold of the prototype.

---
+++ prototyp/src/index.js
@@ -0,0 +1,10 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <StrictMode>
+    <div>Hallo Welt!</div>
+  </StrictMode>,
+  document.getElementById('shopping-list')
+);
