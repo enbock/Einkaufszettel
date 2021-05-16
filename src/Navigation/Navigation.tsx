33e3993acd368b4993b822d7e18a1c06231eca23
Endre Bock <endre.bock@codefrog.de>
2021-05-16 11:13:26 +0200

    EKZ-40 Code improvement

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -16,7 +16,7 @@ interface State {
 }
 
 export default class Navigation extends Component<Properties, State> {
-  private adapter: Adapter;
+  private readonly adapter: Adapter;
 
   constructor(props: Readonly<Properties>) {
     super(props);
